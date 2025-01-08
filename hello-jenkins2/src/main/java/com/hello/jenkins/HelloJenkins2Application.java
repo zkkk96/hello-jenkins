e8680fe834aa169e3b6fa3044474707466969f70
weibin <weibin_cd@163.com>
2025-01-08 09:43:51 +0800

    first commit

---
+++ hello-jenkins2/src/main/java/com/hello/jenkins/HelloJenkins2Application.java
@@ -0,0 +1,18 @@
+package com.hello.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author weibing
+ * @version 1.0.0
+ * @date 2025/1/8
+ * @description
+ */
+@SpringBootApplication
+public class HelloJenkins2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloJenkins2Application.class, args);
+    }
+}
